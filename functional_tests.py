00ffa4064c413ef8d2813b9aded81a65e803b3c4
Vijay Vikrant <vijayvikrant84@gmail.com>
2015-06-08 02:11:45 +0530

    modified the test case to test for behaviour

--- functional_tests.py
+++ functional_tests.py
@@ -18,6 +18,16 @@ class HomePageTest(unittest.TestCase):
     def test_home_page(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header.text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        input_box.send_keys("Buy peacock feathers")
+        input_box.send_keys("\n")
 
         # The below line will always make the test fail
         # force error for tests that are not completed
--- lists/test.py
+++ lists/test.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import home_page
 
 # create your test
-
 
 class HomePageViewTest(TestCase):
 
@@ -13,4 +13,16 @@ class HomePageViewTest(TestCase):
         response = home_page(request)
         self.assertIn('<title>To-Do lists</title>', response.content.decode('utf8'))
         self.assertTrue(response.content.startswith('<html>'))
-        self.assertTrue(response.content.endswith('</html>'))
+        self.assertTrue(response.content.strip().endswith('</html>'))
+
+
+# as much as possible, the tests needs to test behaviour
+
+# instead of checking if the title has something, we could
+# check if the home_page_view is rendering the right template
+# the template is a sort of constant. so redundant to test it.
+        template = render_to_string('home.html')
+        self.assertEqual(
+            response.content.decode('utf8'),
+            template.decode('utf8')
+        )
